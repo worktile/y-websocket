4b4e00cff454634a3703ad12abd126f172e7a022
pubuzhixing8 <pubuzhixing@gmail.com>
2021-07-22 19:13:46 +0800

    feat: add persistence for mongodb

--- bin/server.js
+++ bin/server.js
@@ -6,7 +6,12 @@
 const WebSocket = require('ws')
 const http = require('http')
 const wss = new WebSocket.Server({ noServer: true })
-const setupWSConnection = require('./utils.js').setupWSConnection
+const { MongodbPersistence } = require('y-mongodb');
+const { setupWSConnection, setPersistence } = require('./utils.js')
+
+const location = process.env.MONGODB_URI;
+const collection = 'wiki_pages_transactions';
+const ldb = new MongodbPersistence(location, collection);
 
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 1234
@@ -27,6 +32,36 @@ server.on('upgrade', (request, socket, head) => {
     wss.emit('connection', ws, request)
   }
   wss.handleUpgrade(request, socket, head, handleAuth)
+})
+
+/*
+ Persistence must have the following signature:
+{ bindState: function(string,WSSharedDoc):void, writeState:function(string,WSSharedDoc):Promise }
+*/
+setPersistence({
+  bindState: async (docName, ydoc) => {
+    // Here you listen to granular document updates and store them in the database
+    // You don't have to do this, but it ensures that you don't lose content when the server crashes
+    // See https://github.com/yjs/yjs#Document-Updates for documentation on how to encode 
+    // document updates
+
+    const persistedYdoc = await ldb.getYDoc(docName);
+    const newUpdates = Y.encodeStateAsUpdate(ydoc);
+    ldb.storeUpdate(docName, newUpdates)
+    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
+    ydoc.on('update', async update => {
+      ldb.storeUpdate(docName, update);
+    })
+  },
+  writeState: async (docName, ydoc) => {
+    // This is called when all connections to the document are closed.
+    // In the future, this method might also be called in intervals or after a certain number of updates.
+    return new Prosime(resolve => {
+      // When the returned Promise resolves, the document will be destroyed.
+      // So make sure that the document really has been written to the database.
+      resolve()
+    })
+  }
 })
 
 server.listen({ host, port })
